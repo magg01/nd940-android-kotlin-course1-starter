692b227ea73b89b46f7fc350b7a94a5ff768f64b
Matthew <Magg01@gmail.com>
2021-04-24 17:49:07 +0200

    Revert "ActivityDataBinding object in MainActivity.kt now lateinit var"
    This reverts commit 8a698e7bfae0cec367b7e046ca8041d8ffd435c3.

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -8,14 +8,11 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         Timber.plant(Timber.DebugTree())
     }
